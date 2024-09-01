8f40906a22566070b1bd01aae5dd7ebdcb5c0ae5
Alon Fliess <alonf@codevalue.net>
2024-09-02 02:16:41 +0300

    Enhance project files and XML documentation
    Added properties to generate XML documentation files and suppressed warning 1591. Introduced metadata properties and a conditional `UsePackages` property to toggle between project and NuGet references. Updated `ItemGroup` elements accordingly. Included `README.md` in the package. Corrected XML comments in interfaces. Updated `SemanticKernelPoolingDemo.csproj` with similar changes.

--- SemanticKernelPooling/IKernelPoolRegistration.cs
+++ SemanticKernelPooling/IKernelPoolRegistration.cs
@@ -12,8 +12,7 @@ public interface IKernelPoolRegistration<out TServiceProviderConfiguration> : IK
     /// Registers an action to be executed before a kernel is created. This action allows for 
     /// customization of the kernel building process.
     /// </summary>
-    /// <param name="action">The action to be executed before kernel creation, which takes an <see cref="IKernelBuilder"/> 
-    /// and <see cref="TServiceProviderConfiguration"/> as parameters.</param>
+    /// <param name="action">The action to be executed before kernel creation, which takes an <see cref="IKernelBuilder"/></param>
     /// <exception cref="InvalidOperationException">Thrown if kernels are already present in the pool.</exception>
     void RegisterForPreKernelCreation(Action<IKernelBuilder, TServiceProviderConfiguration, KernelBuilderOptions> action);
 
@@ -30,8 +29,7 @@ public interface IKernelPoolRegistration<out TServiceProviderConfiguration> : IK
     /// Registers an action to be executed after a kernel is created. This action allows for 
     /// additional initialization or configuration of the kernel after it has been built.
     /// </summary>
-    /// <param name="action">The action to be executed after kernel creation, which takes a <see cref="Kernel"/> 
-    /// and <see cref="TServiceProviderConfiguration"/> as parameters.</param>
+    /// <param name="action">The action to be executed after kernel creation, which takes a <see cref="Kernel"/></param>
     /// <exception cref="InvalidOperationException">Thrown if kernels are already present in the pool.</exception>
     void RegisterForAfterKernelCreation(Action<Kernel, TServiceProviderConfiguration> action);
 
